dc9a2614659dfdb7ecc8a8232848ff18707bf160
Entropy Xu <entropy.xcy@hotmail.com>
2023-07-02 00:21:13 -0400

    Add libtorch support and add pytorch_test

---
+++ pytorch_test.h
@@ -0,0 +1,14 @@
+//
+// Created by Entropy Xu on 7/1/23.
+//
+
+#ifndef KMEANS_HACKING_PYTORCH_TEST_H
+#define KMEANS_HACKING_PYTORCH_TEST_H
+
+
+class pytorch_test {
+
+};
+
+
+#endif //KMEANS_HACKING_PYTORCH_TEST_H
