c402cbbfaa7ac0d0fad6b3867d74f9cdbbe32a3d
Entropy Xu <entropy.xcy@hotmail.com>
2023-07-02 22:57:11 -0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
 setup(name='kfloat_cpp',
-      ext_modules=[cpp_extension.CppExtension('kfloat_cpp', ['binding.cpp'])],
+      ext_modules=[cpp_extension.CppExtension('kfloat_cpp', ['binding.cpp', 'util.cpp', 'kmeans.cpp'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
